5ff43fc24ddfbf55cad4d98c44aa63637527f992
lulust03 <stud-lubaba@ruc.dk>
2025-03-06 12:57:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const gameContainer = document.getElementById("game-container");
+
+    gameContainer.addEventListener("click", function () {
+        window.location.href = "førsteSide.html";
+    });
+});
